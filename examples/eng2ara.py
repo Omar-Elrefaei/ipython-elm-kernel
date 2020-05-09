3e239ec6aa917dc940838bcc8e3954bd7007b367
Omar Elrefaei <elrefaei.omar@gmail.com>
2020-05-09 11:56:59 -0400

    Superior implementation using regex

--- examples/eng2ara.py
+++ examples/eng2ara.py
@@ -1,15 +1,24 @@
 import csv
-
+import re
 arWords = []
 enWords = []
-with open('words.csv', newline='') as csvfile:
-    words = csv.reader(csvfile, delimiter=',')
-    for word in words:
-        enWords.append(word[0])
-        arWords.append(word[1])
+with open('enSorted.csv', newline='') as csvfile:
+	words = csv.reader(csvfile, delimiter=',')
+	for word in words:
+		enWords.append(word[0])
+		arWords.append(word[1])
+
+
+
+delms = ["(",")"," ",":","\"","'","=","+","-","!","&","^","*","[","]",";",".",",","/","<",">"]
+temp_str = "("+'|'.join(map(re.escape, delms))+")"
+delms_regex = re.compile(temp_str)
 
 with open('test.py', newline="\n") as codefile:
-        for line in codefile:
-            for (i,enWord) in enumerate(enWords):
-                line = line.replace(enWord, arWords[i])
-            print(line)
+	for line in codefile:
+		words = delms_regex.split(line)
+		for x in range(len(words)):
+			for i in range(len(enWords)):
+				if words[x] == enWords[i]: 
+					words[x] = arWords[i]
+		print(''.join(words))
--- examples/test.py
+++ examples/test.py
@@ -1,1 +1,31 @@
-اطبع("Hello, ELM")
+import re
+
+fruit_list = ['apple', '"', 'peach', 'plum', 'pineapple', 'kiwi']
+fruit = re.compile("("+'|'.join(fruit_list)+")")
+
+import csv
+import re
+arWords = []
+enWords = []
+with open('enSorted.csv', newline='') as csvfile:
+    words = csv.reader(csvfile, delimiter=',')
+    for word in words:
+        enWords.append(word[0])
+        arWords.append(word[1])
+
+
+
+delms = ["(",")"," ",":","\"","'","=","+","-","!","&","^","*","[","]",";",".",",","/","<",">"]
+temp_str = "("+'|'.join(map(re.escape, delms))+")"
+delms_regex = re.compile(temp_str)
+
+with open('test.py', newline="\n") as codefile:
+    for line in codefile:
+        words = delms_regex.split(line)
+        for x in range(len(words)):
+            for i in range(len(enWords)):
+                if words[x] == enWords[i]: 
+                    words[x] = arWords[i]
+        print(''.join(words))
+
+
